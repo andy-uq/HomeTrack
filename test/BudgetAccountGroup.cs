b8c52760112843bd8c0623ff71c89b161e4ca7a8
Andy Clarke <andy.uq@gmail.com>
2012-09-07 17:41:13 +1200

    WIP

--- test/BudgetAccountGroup.cs
+++ test/BudgetAccountGroup.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace HomeTrack.Tests
 {
@@ -6,15 +7,22 @@ namespace HomeTrack.Tests
 	public class BudgetAccountGroupTests
 	{
 		private BudgetAccountGroup _b;
+		private Account _budgetAccount;
+		private Account _expenseAccount;
+		private GeneralLedger _general;
 
 		[SetUp]
 		public void SetUp()
 		{
+			_budgetAccount = AccountFactory.Expense("Grocery Budget");
+			_expenseAccount = AccountFactory.Expense("Groceries");
+			_general = new GeneralLedger(new InMemoryGeneralLedger()) { _budgetAccount, _expenseAccount };
+
 			_b = new BudgetAccountGroup
 			{
-				BudgetAccount = AccountFactory.Expense("Grocery Budget"), 
-				RealAccount = AccountFactory.Expense("Groceries"),
-
+				BudgetAccount = _budgetAccount, 
+				RealAccount = _expenseAccount,
+				BudgetAmount = 100M
 			};
 		}
 
@@ -22,12 +30,84 @@ namespace HomeTrack.Tests
 		public void AllocateBudget()
 		{
 			var budgetAccount = AccountFactory.Expense("Expense Budget");
-			_b.Allocate(budgetAccount);
+			var transaction = _b.Allocate(budgetAccount);
+			
+			var credit = transaction.Credit.Single();
+			Assert.That(credit.Account, Is.EqualTo(budgetAccount));
+			Assert.That(credit.Value, Is.EqualTo(100M));
+			
+			var debit = transaction.Debit.Single();
+			Assert.That(debit.Account, Is.EqualTo(_b.BudgetAccount));
+			Assert.That(credit.Value, Is.EqualTo(100M));
+
+			_general.Post(transaction);
+		}
+
+		[Test]
+		public void PayBudgetedAmount()
+		{
+			var budgetAccount = AccountFactory.Expense("Expense Budget");
+			var allocateTransaction = _b.Allocate(budgetAccount);
+			_general.Post(allocateTransaction);
+
+			var transaction = _b.Pay(50M);
+			_general.Post(transaction);
+	
+			var debit = transaction.Debit.Single();
+			Assert.That(debit.Account, Is.EqualTo(_expenseAccount));
+
+			var credit = transaction.Credit.Single();
+			Assert.That(credit.Account, Is.EqualTo(_budgetAccount));
+
+			Assert.That(_budgetAccount.Balance, Is.EqualTo(50M));
+			Assert.That(_expenseAccount.Balance, Is.EqualTo(50M));
+		}
+
+		[Test]
+		public void GoOverBudgetedAmount()
+		{
+			var budgetAccount = AccountFactory.Expense("Expense Budget");
+			var allocateTransaction = _b.Allocate(budgetAccount);
+			_general.Post(allocateTransaction);
+
+			var transaction = _b.Pay(150M);
+			_general.Post(transaction);
+	
+			var debit = transaction.Debit.Single();
+			Assert.That(debit.Account, Is.EqualTo(_expenseAccount));
+
+			var credit = transaction.Credit.Single();
+			Assert.That(credit.Account, Is.EqualTo(_budgetAccount));
+
+			Assert.That(_budgetAccount.Balance, Is.EqualTo(-50M));
+			Assert.That(_expenseAccount.Balance, Is.EqualTo(150M));
+		}
+
+		[Test]
+		public void EndToEndAmount()
+		{
+			var budgetAccount = AccountFactory.Expense("Expense Budget");
+			var bank = AccountFactory.Asset("Bank");
+
+			var allocateTransaction = _b.Allocate(budgetAccount);
+			_general.Post(allocateTransaction);
+
+
+
+			var transaction = _b.Pay(150M);
+			_general.Post(transaction);
+	
+			var debit = transaction.Debit.Single();
+			Assert.That(debit.Account, Is.EqualTo(_expenseAccount));
+
+			var credit = transaction.Credit.Single();
+			Assert.That(credit.Account, Is.EqualTo(_budgetAccount));
+
+			Assert.That(_budgetAccount.Balance, Is.EqualTo(-50M));
+			Assert.That(_expenseAccount.Balance, Is.EqualTo(150M));
 		}
 	}
-
-
-
+	
 	public class BudgetAccountGroup
 	{
 		public Account BudgetAccount { get; set; }
@@ -37,7 +117,12 @@ namespace HomeTrack.Tests
 
 		public Transaction Allocate(Account budgetAccount)
 		{
-			return new Transaction();
+			return new Transaction(BudgetAccount, budgetAccount, BudgetAmount);
+		}
+
+		public Transaction Pay(decimal value)
+		{
+			return new Transaction(RealAccount, BudgetAccount, value);
 		}
 	}
 }
