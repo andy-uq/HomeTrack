1f3083ca5a1f6b1295a2bea30b696cceec7d9e17
Andy Clarke <andy.uq@gmail.com>
2012-09-07 12:19:14 +1200

    Budgeting

---
+++ test/BudgetAccountGroup.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace HomeTrack.Tests
+{
+	[TestFixture]
+	public class BudgetAccountGroupTests
+	{
+		private BudgetAccountGroup _b;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_b = new BudgetAccountGroup
+			{
+				BudgetAccount = AccountFactory.Expense("Grocery Budget"), 
+				RealAccount = AccountFactory.Expense("Groceries"),
+
+			};
+		}
+
+		[Test]
+		public void AllocateBudget()
+		{
+			var budgetAccount = AccountFactory.Expense("Expense Budget");
+			_b.Allocate(budgetAccount);
+		}
+	}
+
+
+
+	public class BudgetAccountGroup
+	{
+		public Account BudgetAccount { get; set; }
+		public Account RealAccount { get; set; }
+
+		public decimal BudgetAmount { get; set; }
+
+		public Transaction Allocate(Account budgetAccount)
+		{
+			return new Transaction();
+		}
+	}
+}
